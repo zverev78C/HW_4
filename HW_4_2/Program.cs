245227f9fb2568b10fec40ebac20fdf2fa10f160
zverev78 <9842905@gmail.com>
2022-01-24 16:46:02 +0300

    Разделил решение на проекты

---
+++ HW_4_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Заказчику требуется приложение строящее первых N строк треугольника паскаля. N < 25
+            // 
+            // При N = 9. Треугольник выглядит следующим образом:
+            //                                 1
+            //                             1       1
+            //                         1       2       1
+            //                     1       3       3       1
+            //                 1       4       6       4       1
+            //             1       5      10      10       5       1
+            //         1       6      15      20      15       6       1
+            //     1       7      21      35      35       21      7       1
+            //                                                              
+            //                                                              
+            // Простое решение:                                                             
+            // 1
+            // 1       1
+            // 1       2       1
+            // 1       3       3       1
+            // 1       4       6       4       1
+            // 1       5      10      10       5       1
+            // 1       6      15      20      15       6       1
+            // 1       7      21      35      35       21      7       1
+            // 
+            // Справка: https://ru.wikipedia.org/wiki/Треугольник_Паскаля
+        }
+    }
+}
